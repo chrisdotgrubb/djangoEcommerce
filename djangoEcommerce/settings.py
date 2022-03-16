fda68915a7108326c12d8a2542c165e3d3ce3dbf
chrisdotgrubb <developer.grubb@gmail.com>
2022-03-15 21:26:22 -0400

    Setup media and admin

--- djangoEcommerce/settings.py
+++ djangoEcommerce/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +76,15 @@ AUTH_USER_MODEL = 'user.MyUser'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
 USE_TZ = True
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
