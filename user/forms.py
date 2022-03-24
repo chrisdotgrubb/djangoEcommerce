cb1a20a19baf92b71b364eed2f26c855b5147132
chrisdotgrubb <developer.grubb@gmail.com>
2022-03-24 15:10:39 -0400

    Login and Logout

--- user/forms.py
+++ user/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from django.contrib.auth import password_validation
+from django.contrib.auth.forms import AuthenticationForm
 from .models import MyUser
 
 
+class UserLoginForm(AuthenticationForm):
+	username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'login-username'}))
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password', 'id': 'login-password'}))
+	
+	
 class RegistrationForm(forms.ModelForm):
 
 	username = forms.CharField(label='Enter Username: ', min_length=4, max_length=50, help_text='Required')
@@ -45,6 +51,4 @@ class RegistrationForm(forms.ModelForm):
 		if MyUser.objects.filter(email__iexact=email).exists():
 			raise forms.ValidationError('Email already exists.')
 		return email
-		
-		
 		
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,13 @@
 from django. urls import path
+from django.contrib.auth import views as auth_views
 from user import views
+from .forms import UserLoginForm
 
 app_name = 'user'
 
 urlpatterns = [
+	path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', form_class=UserLoginForm), name='login'),
+	path('logout/', auth_views.LogoutView.as_view(next_page='/user/login/'), name='logout'),
 	path('register/', views.user_registration_view, name='register'),
 	path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
 	path('dashboard/', views.dashboard_view, name='dashboard')
