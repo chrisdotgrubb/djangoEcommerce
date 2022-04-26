f7f5d1e90cef96ac8baaaf84d8b9254261ca8d6c
chrisdotgrubb <developer.grubb@gmail.com>
2022-04-26 12:04:29 -0400

    Added delivery choices page.

--- checkout/views.py
+++ checkout/views.py
@@ -1,28 +1,36 @@
-from django.shortcuts import render
 from django.template.response import TemplateResponse
+from django.contrib.auth.decorators import login_required
+from .models import DeliveryOptions
 
 
-
+@login_required
 def delivery_choices_view(request):
-	pass
+	delivery_options = DeliveryOptions.objects.filter(is_active=True)
+	context = {'delivery_options': delivery_options}
+	return TemplateResponse(request, 'checkout/delivery_choices.html', context)
 
 
+@login_required
 def cart_update_delivery_view(request):
 	pass
 
 
+@login_required
 def delivery_address_view(request):
 	pass
 
 
+@login_required
 def payment_selection_view(request):
 	pass
 
 
+@login_required
 def payment_complete_view(request):
 	pass
 
 
+@login_required
 def payment_success_view(request):
 	pass
 
